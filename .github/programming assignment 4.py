c877c24ceaacaac43c588777e8f6a921672a6783
Jonathan Ramos <jonathaniramos360@gmail.com>
2021-11-09 23:06:04 -0500

    validated input

--- .github/programming assignment 4.py
+++ .github/programming assignment 4.py
@@ -29,23 +29,56 @@ def ace_replace(card_list):
     return card_list
 
 
-all_cards = []
+def draw_cards(card_list, player_list, number_of_cards):
+    i = 0
+    while i < number_of_cards:
+        player_list.append(card_list[i])
+        i += 1
+    for i in range(number_of_cards):
+        card_list.pop(0)
+    return player_list
 
-club_cards_13 = ace_replace(str_cards_type(generate_card_list("c"), "c", " of clubs"))
-heart_cards_13 = ace_replace(str_cards_type(generate_card_list("h"), "h", " of hearts"))
-diamond_cards_13 = ace_replace(str_cards_type(generate_card_list("d"), "d", " of diamonds"))
-spade_cards_13 = ace_replace(str_cards_type(generate_card_list("s"), "s", " of spades"))
 
-all_cards.extend(club_cards_13)
-all_cards.extend(heart_cards_13)
-all_cards.extend(diamond_cards_13)
-all_cards.extend(spade_cards_13)
+def validate_input(message):
+    is_valid = False
+    return_message = None
+    while not is_valid:
+        choice = input(message)
+        while not choice.isdigit():
+            choice = input(message)
+        choice = int(choice)
+        is_valid = choice in range(1, 3)
+        return_message = choice
+    return return_message
 
-all_cards = str_cards_type(all_cards, "13", "king")
-all_cards = str_cards_type(all_cards, "12", "queen")
-all_cards = str_cards_type(all_cards, "11", "jack")
 
-print(all_cards)
-print("\n")
-random.shuffle(all_cards)
-print(all_cards)
+def main():
+    all_cards = []
+    dealer_cards = []
+    player_cards = []
+
+    club_cards_13 = ace_replace(str_cards_type(generate_card_list("c"), "c", " of clubs"))
+    heart_cards_13 = ace_replace(str_cards_type(generate_card_list("h"), "h", " of hearts"))
+    diamond_cards_13 = ace_replace(str_cards_type(generate_card_list("d"), "d", " of diamonds"))
+    spade_cards_13 = ace_replace(str_cards_type(generate_card_list("s"), "s", " of spades"))
+
+    all_cards.extend(club_cards_13)
+    all_cards.extend(heart_cards_13)
+    all_cards.extend(diamond_cards_13)
+    all_cards.extend(spade_cards_13)
+
+    all_cards = str_cards_type(all_cards, "13", "king")
+    all_cards = str_cards_type(all_cards, "12", "queen")
+    all_cards = str_cards_type(all_cards, "11", "jack")
+
+    random.shuffle(all_cards)
+    print(all_cards)
+    print("\n")
+
+    player_cards = draw_cards(all_cards, player_cards, 2)
+    print(player_cards)
+
+    player_choice = validate_input("would you like to draw again? (1: yes, 2: no):  ")
+
+
+main()
